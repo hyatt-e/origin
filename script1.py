2d35e7ac605fc17a854528e54904a3468b5d1b4d
gumbopot <hyatt1ea@gmail.com>
2018-07-11 13:16:25 +0000

    Calendar

--- script1.py
+++ script1.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
     return render_template("home.html")
@@ -13,4 +14,4 @@ def about():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=8080)
